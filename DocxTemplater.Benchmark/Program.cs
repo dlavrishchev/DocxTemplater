8987b85e855592f1e65e206e56e0f117b5900be1
dlavrishchev <lavrishchev@gmail.com>
2019-08-01 23:49:51 +0300

    Fix README and other minor changes.

--- DocxTemplater.Benchmark/Program.cs
+++ DocxTemplater.Benchmark/Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.IO;
 using System.Xml.Linq;
+using System.Xml.XPath;
 using BenchmarkDotNet.Running;
 using DocxTemplater.DataSources;
 
 namespace DocxTemplater.Benchmark
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            //ProcessTableTemplate();
-            BenchmarkRunner.Run<TableTemplateBenchmark>();
+            ProcessTableTemplate();
+            //BenchmarkRunner.Run<TableTemplateBenchmark>();
         }
 
         private static void ProcessTableTemplate(int rowsCount = 1_000)
@@ -18,14 +20,8 @@ namespace DocxTemplater.Benchmark
             int cursorTop, cursorLeft;
 
             Console.WriteLine("Start process table template...");
-            var datasetPath = @".\table_dataset.xml";
-
             Console.Write("Generating dataset...");
-            GenerateDataset();
-            Console.WriteLine("done");
-
-            Console.Write("Loading datasource...");
-            var ds = XmlDataSource.Create(datasetPath);
+            var ds = GenerateDataset(rowsCount:1000);
             Console.WriteLine("done");
 
             Console.Write("Processing template...");
@@ -35,7 +31,6 @@ namespace DocxTemplater.Benchmark
             using (var doc = new WordDocument(@".\templates\table_template.docx"))
             {
                 doc.ProcessMainDocumentContentControls(ds);
-                doc.ProcessFootersContentControls(ds);
 
                 Console.WriteLine("\nSave result to file? [y/n]");
                 if(Console.ReadLine()?.ToLower() == "y")
@@ -45,28 +40,32 @@ namespace DocxTemplater.Benchmark
             Console.CursorTop = cursorTop;
             Console.CursorLeft = cursorLeft;
             Console.WriteLine("done");
+        }
 
-            void GenerateDataset()
+        private static IXPathNavigable GenerateDataset(int rowsCount)
+        {
+            var items = new XElement[rowsCount];
+            for (var i = 0; i < rowsCount; i++)
             {
-                var items = new XElement[rowsCount];
-                for (var i = 0; i < rowsCount; i++)
-                {
-                    var item = new XElement("item",
-                        new XElement("c1", i, new XAttribute("textColor", "#FF0000")),
-                        new XElement("c2", i, new XAttribute("highlightColor", "green")),
-                        new XElement("c3", i, new XAttribute("bold", "true")),
-                        new XElement("c4", i, new XAttribute("italic", "true")),
-                        new XElement("c5", i, new XAttribute("underline", "true")),
-                        new XElement("c6", i),
-                        new XElement("c7", i),
-                        new XElement("c8", i),
-                        new XElement("c9", i),
-                        new XElement("c10", i));
-                    items[i] = item;
-                }
-                var doc = new XDocument(
-                    new XElement("items", items));
-                doc.Save(datasetPath);
+                var item = new XElement("item",
+                    new XElement("c1", i, new XAttribute("textColor", "#FF0000")),
+                    new XElement("c2", i, new XAttribute("highlightColor", "green")),
+                    new XElement("c3", i, new XAttribute("bold", "true")),
+                    new XElement("c4", i, new XAttribute("italic", "true")),
+                    new XElement("c5", i, new XAttribute("underline", "true")),
+                    new XElement("c6", i),
+                    new XElement("c7", i),
+                    new XElement("c8", i),
+                    new XElement("c9", i),
+                    new XElement("c10", i));
+                items[i] = item;
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                new XElement("items", items).Save(ms);
+                ms.Position = 0;
+                return new XPathDocument(ms);
             }
         }
     }
--- README.md
+++ README.md
@@ -2,32 +2,62 @@
 DocxTemplater
 </h1>
 
-Lightweight library for generating .docx files from a template and xml data.
+Fast library written in C# for generating .docx files from a template and xml datasets.
 
-- Based on Word content control features and XPath expressions.
+- Based on MS Office Word content control feature and XPath expressions.
 
 - Without external dependencies.
 
 - .NET Standard 2.0.
 
+### Features
+- Filling tables and fields.
+
 ### Usage
 
 ```csharp
 using DocxTemplater;
-using DocxTemplater.DataSources;
 
-var dataSource = new XmlDataSource();
-dataSource.Load(@".\files\data.xml");
-
-using (var tp = new TemplateProcessor(@".\files\template.docx"))
+static class Program
 {
-	tp.Process(dataSource);
-	tp.Save(@".\files\result.docx");
+    static void Main(string[] args)
+    {
+        var dataset = GenerateDataset(rowsCount:1000);
+        using (var doc = new WordDocument(@".\files\template.docx"))
+        {
+	    doc.ProcessAllContentControls(dataset);
+	    doc.Save(@".\files\result.docx");
+        }
+    }
+
+    private static IXPathNavigable GenerateDataset(int rowsCount)
+    {
+        var items = new XElement[rowsCount];
+        for (var i = 0; i < rowsCount; i++)
+        {
+            var item = new XElement("item",
+                new XElement("c1", i, new XAttribute("textColor", "#FF0000")),
+                new XElement("c2", i, new XAttribute("highlightColor", "green")),
+                new XElement("c3", i, new XAttribute("bold", "true")),
+                new XElement("c4", i, new XAttribute("italic", "true")),
+                new XElement("c5", i, new XAttribute("underline", "true")),
+                new XElement("c6", i),
+                new XElement("c7", i),
+                new XElement("c8", i),
+                new XElement("c9", i),
+                new XElement("c10", i));
+            items[i] = item;
+        }
+
+        using (var ms = new MemoryStream())
+        {
+            new XElement("items", items).Save(ms);
+            ms.Position = 0;
+            return new XPathDocument(ms);
+        }
+    }
 }
 ```
-[template.docx](tests/DocxTemplater.Demo/demo_files/template.docx)<br>
-[result.docx](tests/DocxTemplater.Demo/demo_files/result.docx)<br>
-[data.xml](tests/DocxTemplater.Demo/demo_files/data.xml)
 
 ### Text styling  XML attributes
 
@@ -37,12 +67,4 @@ using (var tp = new TemplateProcessor(@".\files\template.docx"))
 |italic		  |true / false		 |
 |underline	  |true / false		 |
 |textColor	  |Color hex code in format '#RRGGBB'. For example, #005ce6	 |
-|textHighlightColor	  |black<br>blue<br>darkBlue<br>cyan<br>darkCyan<br>magenta<br>darkMagenta<br>red<br>darkRed<br>white<br>yellow<br>darkYellow<br>green<br>darkGreen<br>darkGray<br>lightGray|
-
-```xml
-<album>
-    <name textColor="#005ce6" bold="true">Wish You Were Here</name>
-    <label>Harvest</label>
-    <year textHighlightColor="red">1975</year>
-</album>
-```
+|highlightColor	  |black<br>blue<br>darkBlue<br>cyan<br>darkCyan<br>magenta<br>darkMagenta<br>red<br>darkRed<br>white<br>yellow<br>darkYellow<br>green<br>darkGreen<br>darkGray<br>lightGray|
